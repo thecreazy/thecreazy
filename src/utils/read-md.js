c8bc761ef80cdc68e10736bb01349f8cb6749c8c
thecreazy <canella_riccardo@icloud.com>
2023-11-09 21:27:41 +0100

    feat(site): more parsing options on md file

--- src/utils/read-md.js
+++ src/utils/read-md.js
@@ -2,7 +2,7 @@ const showdown  = require('showdown');
 const fs = require("fs");
 
 function readMD(path){
-    const converter = new showdown.Converter();
+    const converter = new showdown.Converter({smartIndentationFix: true, openLinksInNewWindow: true, moreStyling:true, customizedHeaderId: true});
     const mdText = fs.readFileSync(path, "utf-8");
     const html  = converter.makeHtml(mdText);
     return html
