942deb200a2515f26be16102838fe3d557bd6431
thecreazy <canella_riccardo@icloud.com>
2026-03-23 17:34:32 +0100

    feat(core): added robot, og image and sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://canellariccardo.it',
+  integrations: [tailwind(), sitemap()],
   output: 'static',
   vite: {
     assetsInclude: ['**/*.glsl'],
